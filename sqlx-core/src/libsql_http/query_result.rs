7bf5e3b77656c19fd699c9c5c664e9bb64a34e92
Piotr Sarna <sarna@chiselstrike.com>
2023-01-30 14:26:04 +0100

    query_result

--- sqlx-core/src/libsql_http/query_result.rs
+++ sqlx-core/src/libsql_http/query_result.rs
@@ -1,1 +1,10 @@
+use std::iter::{Extend, IntoIterator};
 
+#[derive(Debug, Default)]
+pub struct LibsqlHttpQueryResult {
+}
+
+impl Extend<LibsqlHttpQueryResult> for LibsqlHttpQueryResult {
+    fn extend<T: IntoIterator<Item = LibsqlHttpQueryResult>>(&mut self, iter: T) {
+    }
+}
